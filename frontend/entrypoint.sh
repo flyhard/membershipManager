6f198eee170b6f3b9d5b8a0131e62cbeafe39ab5
Per Abich <per.abich@gmail.com>
2015-01-14 15:52:28 +0100

    Improved entrypoint script

--- frontend/entrypoint.sh
+++ frontend/entrypoint.sh
@@ -4,4 +4,13 @@ set -e
 
 echo $*
 
+if [[ -z "$1" ]]
+then
+    if [[ -z "$DROPWIZARD_PORT_8080_TCP_ADDR" -o -z "DROPWIZARD_PORT_8080_TCP_PORT" ]]
+    then
+        echo "Missing link to DROPWIZARD container. Please run with --link <name>:dropwizard"
+        exit 1
+    fi
+fi
+
 exec "$@"
